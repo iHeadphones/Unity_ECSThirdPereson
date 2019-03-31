7acd4f3635180d619a23c5404c2169f8a6802ae5
Little coding wolf <nicholasgennady@outlook.com>
2019-03-31 14:29:38 -0500

    Added grass system comments

--- Project/Assets/Source/TPCC/Systems/GrassUpdateSystem.cs
+++ Project/Assets/Source/TPCC/Systems/GrassUpdateSystem.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 
 public class GrassUpdateSystem : ComponentSystem
 {
+    //Blueprint of what grass data is made out
     public struct GrassData
     {
         public readonly int Length;
@@ -19,6 +20,7 @@ public class GrassUpdateSystem : ComponentSystem
         public EntityArray Entity;
     }
 
+    //BLueprint of entites with grass collisions
     public struct GrassCollisionData
     {
         public readonly int Length;
@@ -26,8 +28,11 @@ public class GrassUpdateSystem : ComponentSystem
         public ComponentArray<Transform> Transform;
     }
 
+    //Inject to get actors from blueprints
     [Inject] private GrassData grassData;
     [Inject] private GrassCollisionData grassCollisionData;
+
+    //Helpful Variables
     private Dictionary<int, float> grassIntervals = new Dictionary<int, float>();
     private List<GameObject> grassGameObjectsToDestroy = new List<GameObject>();
     private MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
@@ -36,35 +41,40 @@ public class GrassUpdateSystem : ComponentSystem
     protected override void OnUpdate()
     {
 
+        //Loop threw all grass entines
         for (int i = 0; i < grassData.Length; i++)
         {
+            //Get grass entity data
             var grass = grassData.Grass[i];
             var grassRenderer = grassData.MeshRenderer[i];
             var grassTransform = grassData.Transform[i];
             var grassEntity = grassData.Entity[i];
-
-            grassRenderer.GetPropertyBlock(materialPropertyBlock);
-
+        
+            //Keep track of grass intevals by entity ID
             if (!grassIntervals.ContainsKey(grassEntity.Index))
-            {
-
                 grassIntervals.Add(grassEntity.Index, 0);
 
-            }
+            //Get Current Materail property block for grass
+            grassRenderer.GetPropertyBlock(materialPropertyBlock);
 
+            //Loop threw all entities with grass Collision
             for (int j = 0; j < grassCollisionData.Length; j++)
             {
+                //Get Grass Collision entity data
                 var grassCollsion = grassCollisionData.GrassCollision[j];
                 var grassCollisionTransform = grassCollisionData.Transform[j];
 
+                //Check if werew colliding with grass based on a distance check
                 if (Vector3.Distance(grassTransform.position, grassCollisionTransform.position) <= grassCollsion.distance)
                 {
-
-                    grassIntervals[grassEntity.Index] = grass.bendUpDelay;
+                    //Bend Grass
                     var direction = (grassTransform.position - grassCollisionTransform.position).normalized;
                     direction *= grass.bendAmount;
                     direction.y = -grass.stampAmount;
                     materialPropertyBlock.SetVector(offsetID, Vector3.Slerp(materialPropertyBlock.GetVector(offsetID), direction, grass.bendDownSpeed));
+
+                    //Reset Grass Bend interval
+                    grassIntervals[grassEntity.Index] = grass.bendUpDelay;
 
                     //PLACE HOLDER CODE FOR GRASS CUTTING BY PRESSING KEY AND IN RADIUS
                     if (Input.GetKeyDown(KeyCode.Q))
@@ -75,9 +85,10 @@ public class GrassUpdateSystem : ComponentSystem
                         PostUpdateCommands.DestroyEntity(grassEntity);
                     }
                 }
+
+                //Bend Grass Back up when interval is 0
                 else
                 {
-
                     grassIntervals[grassEntity.Index] -= 1 * Time.deltaTime;
 
                     if (grassIntervals[grassEntity.Index] <= 0)
